088665d692fcdc0f2ad98845fb60680c9a4e643f
raptorVladimirAdigeya <98263225+raptorVladimirAdigeya@users.noreply.github.com>
2022-02-03 03:17:27 +0300

    Add files via upload

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,34 @@
+<template>
+        <nav class="navbar navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a href="#" class="navbar-brand">Marvel</a>
+                <form class="d-flex">
+                    <input type="search"
+                        placeholder="Поиск..."
+                        aria-label="Search"
+                        class="form-control me-2"
+                    >
+                    <button
+                            type="reset"
+                            class="btn btn-outline-light"
+                    >
+                        все
+                    </button>
+                </form>
+            </div>
+        </nav>
+</template>
+
+<script>
+    export default {
+        name: "AppHeader",
+        data() {
+            return {
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
